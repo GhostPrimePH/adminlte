ff24dc2c6f643ef8c268f7b4a16b24b2cd6edcfd
GhostPrimePH <predatorstrikesyt@gmail.com>
2025-05-14 20:07:37 +0800

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">User Management</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarSupportContent"
+        aria-controls="navbarSupportContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportContent">
+
+    </div>
+
+</nav>
